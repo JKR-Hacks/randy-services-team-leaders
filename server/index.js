225ee60c3d8686a54bb1f792df3ca1e71d3fca0c
Randy <rlthomasccv@gmail.com>
2019-01-02 20:54:12 -0800

    SKELETON

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../database/index.js');
+
+const app = express();
+
+app.use(express.static(`${__dirname}/../client/dist`));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.get('/stats', (req, res) => {
+  db.Stats.find({}).exec((err, data) => {
+    res.send(data);
+  });
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
